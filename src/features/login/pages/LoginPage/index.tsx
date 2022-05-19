74708cc3524521959e71aae84a075ff8e2d862e8
Daniel Kraus <danielkraus@Daniel-Kraus-MacBookPro.local>
2022-05-19 16:17:34 +0200

    feat: add form to Login page

--- src/features/login/pages/LoginPage/index.tsx
+++ src/features/login/pages/LoginPage/index.tsx
@@ -1,9 +1,25 @@
 import type { NextPage } from 'next'
+import type { FormEvent } from 'react'
 
+import { Input } from '~/features/ui/components/Input'
 import { LayoutPublic } from '~/features/ui/components/LayoutPublic'
 
-export const LoginPage: NextPage = () => (
-  <LayoutPublic>
-    <h1>This is Login Page!</h1>
-  </LayoutPublic>
-)
+export const LoginPage: NextPage = () => {
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    alert('TODO')
+  }
+
+  return (
+    <LayoutPublic>
+      <h1>Sign in to Eventio.</h1>
+      <p>Enter your details below.</p>
+      <form onSubmit={onSubmit}>
+        <Input label="Email" type="email" name="email" />
+        <Input label="Password" type="password" name="password" />
+        <button type="submit">Sign In</button>
+      </form>
+    </LayoutPublic>
+  )
+}
