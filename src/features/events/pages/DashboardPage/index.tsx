bb987505651dac72c92c8777fea681e350df664e
Daniel Kraus <danielkraus@Daniel-Kraus-MacBookPro.local>
2022-05-19 17:52:27 +0200

    feat: wrap Events in Dashboard to section

--- src/features/events/pages/DashboardPage/index.tsx
+++ src/features/events/pages/DashboardPage/index.tsx
@@ -8,9 +8,10 @@ import { CreateFAB } from '../../components/EventsList/parts/CreateFAB'
 export const DashboardPage: NextPage = () => (
   <Layout>
     <h1>Dashboard</h1>
-    <h2>Events List</h2>
-    <EventsList />
-
-    <CreateFAB />
+    <section>
+      <h2>Events List</h2>
+      <EventsList />
+      <CreateFAB />
+    </section>
   </Layout>
 )
