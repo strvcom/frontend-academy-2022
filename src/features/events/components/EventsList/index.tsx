d545952da244f7f8ee46b30d135b5c00e093695e
hofik9 <honza.hoffman@gmail.com>
2022-06-06 11:06:59 +0200

    feat(EventList): useState for toggling layout

--- src/features/events/components/EventsList/index.tsx
+++ src/features/events/components/EventsList/index.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react'
+import { useState } from 'react'
 
 import { EventItem } from './parts/EventItem'
 import { NavigationFilter } from './parts/NavigationFilter'
@@ -7,55 +8,52 @@ import { List, Nav } from './styled'
 import { ViewType } from './types'
 
 export const EventsList: FC = () => {
-  const view = ViewType.GRID as ViewType
+  const [view, setView] = useState<ViewType>(ViewType.GRID)
 
   return (
     <>
       <Nav>
         <NavigationFilter onChange={(filterType) => alert(filterType)} />
-        <NavigationView
-          activeView={view}
-          onChange={(viewType) => alert(viewType)}
-        />
+        <NavigationView activeView={view} onChange={setView} />
       </Nav>
 
       <List view={view}>
         <li>
           <EventItem isRow={view === ViewType.LIST} />
         </li>
-        
+
         <li>
           <EventItem isRow={view === ViewType.LIST} />
         </li>
-        
+
         <li>
           <EventItem isRow={view === ViewType.LIST} />
         </li>
-        
+
         <li>
           <EventItem isRow={view === ViewType.LIST} />
         </li>
-        
+
         <li>
           <EventItem isRow={view === ViewType.LIST} />
         </li>
-        
+
         <li>
           <EventItem isRow={view === ViewType.LIST} />
         </li>
-        
+
         <li>
           <EventItem isRow={view === ViewType.LIST} />
         </li>
-        
+
         <li>
           <EventItem isRow={view === ViewType.LIST} />
         </li>
-        
+
         <li>
           <EventItem isRow={view === ViewType.LIST} />
         </li>
-        
+
         <li>
           <EventItem isRow={view === ViewType.LIST} />
         </li>
