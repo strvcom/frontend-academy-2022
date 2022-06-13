3a7510482050944ebbed6e366ec3f256f904417d
Dan Balarin <dan.balarin@gmail.com>
2022-06-13 18:20:29 +0200

    feat: updated api client to use environment variables

--- src/features/api/lib/client.ts
+++ src/features/api/lib/client.ts
@@ -1,16 +1,29 @@
 import type { BeforeRequestInterceptor } from './network-provider'
 import { NetworkProvider } from './network-provider'
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL
+const apiKey = process.env.NEXT_PUBLIC_API_KEY
+
+// Safeguard the application isn't initiated without NEXT_PUBLIC_API_URL
+if (!apiUrl) {
+  throw new Error('Missing NEXT_PUBLIC_API_URL environment variable')
+}
+
+// Safeguard the application isn't initiated without NEXT_PUBLIC_API_KEY
+if (!apiKey) {
+  throw new Error('Missing NEXT_PUBLIC_API_KEY environment variable')
+}
+
 /**
  * Before request hook to append API Key header on all requests.
  */
 const appendAPIKey: BeforeRequestInterceptor = (request) => {
-  request.headers.append('APIKey', 'YOUR_KEY')
+  request.headers.append('APIKey', apiKey)
   return request
 }
 
 const api = new NetworkProvider({
-  baseUrl: 'https://testproject-api-v2.strv.com/',
+  baseUrl: apiUrl,
   interceptors: {
     beforeRequest: [appendAPIKey],
   },
