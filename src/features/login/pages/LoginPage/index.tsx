1bb71660eef8fa42c0956adc8fcd537a6effc750
Lukas Polak <lukash.polak@gmail.com>
2022-06-17 09:16:44 +0200

    chore: custom validation messages for yup

--- src/features/login/pages/LoginPage/index.tsx
+++ src/features/login/pages/LoginPage/index.tsx
@@ -21,10 +21,20 @@ type FormInputs = {
   password: number
 }
 
+// Validation messages are optional
+// yup already have some predefined validation messages
+// See more: https://github.com/jquense/yup/blob/a8febddcfbe42358e63194ae8da582e66b746edf/src/locale.ts
 const schema = yup
   .object({
-    email: yup.string().email().required(),
-    password: yup.string().min(8).max(16).required(),
+    email: yup
+      .string()
+      .email('Email must be a valid email')
+      .required('Email is a required field'),
+    password: yup
+      .string()
+      .min(8, 'Password must be at least 8 characters')
+      .max(16, 'Password must be at most 16 characters')
+      .required('Password is a required field'),
   })
   .required()
 
