9fb2fb611c4323ccc4be67578b5b78abb8fc73a3
michaljarnot <m.jarnot@yahoo.com>
2022-06-24 21:14:02 +0200

    build: run next server in cypress process

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,30 @@
-/* eslint-disable import/no-default-export,@typescript-eslint/no-unused-vars */
+/* eslint-disable import/no-default-export,@typescript-eslint/no-unused-vars,@typescript-eslint/no-misused-promises */
 import { defineConfig } from 'cypress'
+import http from 'http'
+import next from 'next'
+
+const BASE_URL = 'http://localhost:3000'
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: BASE_URL,
+    setupNodeEvents: async (on, config) => {
+      const app = next({ dev: true })
+      const handleNextRequests = app.getRequestHandler()
+      await app.prepare()
+
+      const customServer = new http.Server(async (req, res) => {
+        return await handleNextRequests(req, res)
+      })
+
+      await new Promise<void>((resolve) => {
+        customServer.listen(3000, () => {
+          console.log(`> Ready on ${BASE_URL}`)
+          resolve()
+        })
+      })
+
+      return config
     },
-    baseUrl: 'http://localhost:3000',
   },
 })
